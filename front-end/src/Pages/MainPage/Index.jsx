4de7849a074a6eb9a946292ecf72e9615edcb465
Gabriel Borges <gabriel.moura2009@hotmail.com>
2022-10-06 15:27:14 -0300

    style: adjusting form

--- front-end/src/Pages/MainPage/Index.jsx
+++ front-end/src/Pages/MainPage/Index.jsx
@@ -1,13 +1,14 @@
-import Button from "../../Components/Button/Index";
 import Container from "../../Components/Container/Index";
 
 function MainPage() {
     return(
         <Container>
             <h1>Página Principal</h1>
+
+            <br/>
+
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae accusantium eveniet corporis? In hic aliquam odit. Facilis sed natus quas itaque assumenda voluptas voluptatem, ipsa autem. Dicta, quam quaerat? Illum?</p>
 
-            <Button text="Enviar"/>
         </Container>
     );
 }
